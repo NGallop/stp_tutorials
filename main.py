80f973ce7d44c6819cd5cd8a43d9aa836154d9b6
NGallop <n.p.gallop@gmail.com>
2023-08-24 10:41:35 +0100

    files structured ready for functions to be written

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+###############################################################################
+# Author: Niall Gallop                                                        #
+# Date:                                                                       #
+# Name: Main.py                                                               #
+# Purpose: Handle exercise 2 objects                                          #
+###############################################################################
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
+import src.exercise_2 as ex2
+import src.mould_sequence as mld
 
+def main():
+    # outline argparse arguments
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument(
+        '-i', '--input', required = True, help='Input file')
+    parser.add_argument(
+        '-f', '--genbank_format', action='store_true',
+        help='Reformat the sequence in gebank format')
+    parser.add_argument(
+        '-p', '--protein', action='store_true',
+          help='Translate DNA sequence to amino acid sequence')
+    parser.add_argument(
+        '-c', '--reverse_complement', action='store_true',
+          help='Convert sequence to the reverse complement')
+    parser.add_argument(
+        '-r', '--reading_frames', action='store_true',
+          help='Return all 6 reading frames for a given sequence')
+    parser.add_argument(
+        '-b', '--base_counting', action='store_true',
+          help='Count all mono-, di-, and tri-nucleotide instances in a seq.')
+    parser.add_argument(
+        '-g', '--gc_content', action='store_true',
+          help='Return the GC-content of a given sequence as a percentage')
+    args = parser.parse_args()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # open and read the txt file containing sequence
+    sequence = open(args.input, 'r')
+    sequence = sequence.read()
 
+    # initialise mold sequence by passing the raw sequence in
+    mould_obj = mld.mould_sequence(sequence)
+    # using mol_sequence, return a corrected standard sequence format
+    corrected_seq = mould_obj.seq_mould() #add seq_errors
+    # print non-conformities and corrective actions to the terminal
+    #print(seq_errors)
 
-# Press the green button in the gutter to run the script.
+    # initialise exercise 2 object
+    ex2_obj = ex2.exercise_2(corrected_seq)
+    # perform desired operations
+    if args.genbank_format == True:
+        genbank_format = ex2_obj.genbank_convert()
+    if args.protein == True:
+        amino_acids = ex2_obj.DNA_to_protein()
+    if args.reverse_complement == True:
+        reverse = ex2_obj.reverse_seq()
+    if args.reading_frames == True:
+        six_frame = ex2_obj.sixFrame_translate()
+    if args.base_counting == True:
+        base_counts = ex2_obj.count_bases()
+    if args.gc_content == True:
+        gc_content = ex2_obj.gc_content()
+    # add if no arguments selected consequence here
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- src/exercise_2.py
+++ src/exercise_2.py
@@ -1,14 +1,43 @@
-import argparse
+###############################################################################
+# Author: Niall Gallop                                                        #
+# Date:                                                                       #
+# Name: exercise2.py                                                          #
+# Purpose: Handle exercise 2 methods within an object                         #
+###############################################################################
 
-parser = argparse.ArgumentParser(
-    description='')
-parser.add_argument(
-    '-i', '--input', required = True, help='Input file')
-parser.add_argument(
-    '-o', '--output', required=True, help='Output file')
-parser.add_argument(
-    '-e', '--example_1', action='store_true', help='')
-parser.add_argument(
-    '-f', '--example_2', action='store_true', help='')
 
-args = parser.parse_args()
+# object to house all exercise 2 methods
+class exercise_2():
+
+    def __init__(self, incoming_sequence):
+        self.sequence = incoming_sequence
+
+    # 2.1: convert fasta DNA base string to GenBank records style. 
+    # e.g. "ACTGACTGACTGACTGACTG" > '1 actgactgac tgactgactg actgactgac'
+    def genbank_convert(self):
+        return
+    
+    # 2.2: translate DNA sequence to amino acid sequence
+    # e.g. aggagtaag > RSK, or AGGAGTAAG > RSK
+    def DNA_to_protein(self):
+        return
+    
+    # 2.3 generate the reverse complement of a sequence
+    # e.g. aggagtaag > cttactcct
+    def reverse_seq(self):
+        return
+    
+    # 2.4 generate a six-frame translation of a sequence
+    # e.g. Forward: RSK, GVS, E*A; Reverse: LTP, GLL, AYS
+    def sixFrame_translate(self):
+        return
+    
+    # 2.5 count mono-, di- and tri-nucleotides in a sequence
+    # e.g. a 12, g 9, t 7... ag 1, ga 1, gt 2, aa 3... acg 1, att 1, agt 1...
+    def count_bases(self):
+        return
+    
+    # 2.6 generate GC-content % of a sequence
+    # e.g. acctaggccat > GC-content = 50%
+    def gc_content(self):
+        return
