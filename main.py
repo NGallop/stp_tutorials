cca305fc6353402ed0c5706ab838645f172f06e0
NGallop <n.p.gallop@gmail.com>
2023-08-29 09:06:34 +0100

    added some defensive fuinctionality to main.py to handle some predicted exceptions

--- main.py
+++ main.py
@@ -16,22 +16,33 @@ def main():
     # initialise cli object
     cli_obj = cli.cli_obj(sys.argv[1:])
     selected_args = cli_obj.arg_selection()
+    # check if arguments have been selected and abort program if not
     if selected_args == [False, False, False, False, False, False]:
         print("No arguments selected, aborting program")
         quit()
 
     # open and read the txt file containing sequence
-    sequence = open(cli_obj.args.input, 'r')
-    sequence = sequence.read()
+    try:
+        sequence = open(cli_obj.args.input, 'r')
+        sequence = sequence.read()
+    except FileNotFoundError:
+        print("ERROR: input file not found, please check input")
+        quit()
+
+    # hardcoded codon table
+    quitter = 0
+    try:
+        with open ('ref/codon.json', 'r') as f:
+            codon_json = json.load(f)
+    except FileNotFoundError:
+        print("Warning: could not load codon table. Codon table must be located at ref/codon.json")
+        codon_json = {}
+        quitter = 1
 
     # initialise mold sequence by passing the raw sequence in
     mould_obj = mld.mould_sequence(sequence)
     # using mol_sequence, return a corrected standard sequence format
     corrected_seq = mould_obj.seq_mould() 
-
-    # hardcoded codon table
-    with open ('ref/codon.json', 'r') as f:
-        codon_json = json.load(f)
     
     # initialise exercise 2 object
     ex2_obj = ex2.exercise_2(corrected_seq, codon_json)
@@ -39,16 +50,24 @@ def main():
     if selected_args[0] == True:
         genbank_format = ex2_obj.genbank_convert()
         print(genbank_format)
-    if selected_args[1]== True:
-        amino_acids = ex2_obj.DNA_to_protein()
-        print(amino_acids)
+    if selected_args[1] == True:
+        if quitter > 0:
+            print('ERROR: Codon table required for function. Program aborted.')
+            quit()
+        else:
+            amino_acids = ex2_obj.DNA_to_protein()
+            print(amino_acids)
     if selected_args[2] == True:
         reverse = ex2_obj.reverse_seq()
         print('Forward:\n', corrected_seq)
         print('Reverse:\n', reverse)
     if selected_args[3] == True:
-        six_frame = ex2_obj.sixFrame_translate()
-        print(six_frame)
+        if quitter > 0:
+            print('ERROR: Codon table required for function. Program aborted.')
+            quit()
+        else:
+            six_frame = ex2_obj.sixFrame_translate()
+            print(six_frame)
     if selected_args[4] == True:
         base_counts = ex2_obj.count_bases()
         print("base counts: ", base_counts)
